7c0cbd21f9a09691f0c946dbcae47e63646cdd19
keita7777 <47020745+keita7777@users.noreply.github.com>
2024-11-19 10:03:03 +0900

    プロフィールメニュー作成 (#21)
    Co-authored-by: github keita7777 <github mine0kei@gmail.com>

--- components/Header/HeaderNav/ProfileMenu/index.tsx
+++ components/Header/HeaderNav/ProfileMenu/index.tsx
@@ -1,4 +1,10 @@
 const ProfileMenu = () => {
-  return <div>ProfileMenu</div>;
+  return (
+    <div className="flex flex-col">
+      <button className="flex justify-center items-center gap-4 rounded-md text-white bg-red-600 p-2 hover:bg-red-500 duration-100">
+        ログアウト
+      </button>
+    </div>
+  );
 };
 export default ProfileMenu;
