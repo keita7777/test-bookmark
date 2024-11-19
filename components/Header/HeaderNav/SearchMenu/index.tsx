a2b7dc752be0ff401c21872f17f6b803d068c059
keita7777 <47020745+keita7777@users.noreply.github.com>
2024-11-19 09:52:36 +0900

    検索メニュー作成 (#20)
    Co-authored-by: github keita7777 <github mine0kei@gmail.com>

--- components/Header/HeaderNav/SearchMenu/index.tsx
+++ components/Header/HeaderNav/SearchMenu/index.tsx
@@ -1,4 +1,20 @@
+import { FaSearch } from "react-icons/fa";
+
 const SearchMenu = () => {
-  return <div>SearchMenu</div>;
+  return (
+    <form className="flex flex-col gap-4">
+      <input type="text" placeholder="キーワードを入力" className="rounded-md outline-none p-2" />
+      <div className="flex flex-col gap-2">
+        <button className="flex justify-center items-center gap-4 rounded-md bg-gray-100 p-2 hover:bg-gray-300 duration-100">
+          現在のフォルダから検索
+          <FaSearch />
+        </button>
+        <button className="flex justify-center items-center gap-4 rounded-md bg-gray-100 p-2 hover:bg-gray-300 duration-100">
+          全てのフォルダから検索
+          <FaSearch />
+        </button>
+      </div>
+    </form>
+  );
 };
 export default SearchMenu;
