d57975683e8821bebff4abfc9da77bcc7918fddf
keita7777 <47020745+keita7777@users.noreply.github.com>
2024-11-23 06:36:00 +0900

    ブックマーク作成ボタン (#29)
    * ブックマーク作成ボタン
    
    * 不要なdivタグ削除
    
    ---------
    
    Co-authored-by: github keita7777 <github mine0kei@gmail.com>

--- app/(bookmark)/page.tsx
+++ app/(bookmark)/page.tsx
@@ -1,10 +1,12 @@
 // 全てのブックマークを表示する画面
 
+import CreateButton from "@/components/Bookmark/CreateButton";
 import BookmarkList from "@/components/Bookmark/List";
 
 export default function Home() {
   return (
     <>
+      <CreateButton />
       <BookmarkList />
     </>
   );
