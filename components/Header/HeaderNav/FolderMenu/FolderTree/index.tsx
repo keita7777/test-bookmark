29fb6e89a0a008ee4400e7ef767270102a73bad8
keita7777 <47020745+keita7777@users.noreply.github.com>
2024-11-27 08:15:45 +0900

    foldersが存在するときのみFolderTree表示 (#69)
    Co-authored-by: github keita7777 <github mine0kei@gmail.com>

--- components/Header/HeaderNav/FolderMenu/FolderTree/index.tsx
+++ components/Header/HeaderNav/FolderMenu/FolderTree/index.tsx
@@ -11,33 +11,33 @@ type Props = {
 };
 
 const FolderTree = ({ folders, parentId = null, openFolders, toggleFolder }: Props) => {
-  return (
-    folders
-      // フォルダデータの親フォルダ（parent_folder）が受け取ったparentIdと一致するフォルダをフィルター
-      // parentIdがない場合は親フォルダが存在しないフォルダ（第1階層のフォルダ）を取得する
-      .filter((folder) => folder.parent_relation.parent_folder === parentId)
-      .map((folder) => (
-        <li id={folder.id} key={folder.id} className="flex flex-col gap-5">
-          <FolderItem
-            folder={folder}
-            folderData={folders}
-            isSubFolderVisible={!!openFolders[folder.id]}
-            toggleFolder={() => toggleFolder(folder.id)}
-          />
-          {/* フォルダに子フォルダがある場合、再帰的にFolderTreeを呼び出す */}
-          {folder.parent_relation.hasChild && openFolders[folder.id] && (
-            <ul className="ml-6 flex flex-col gap-5">
-              <FolderTree
-                folders={folders}
-                parentId={folder.id}
-                openFolders={openFolders}
-                toggleFolder={toggleFolder}
-              />
-            </ul>
-          )}
-        </li>
-      ))
-  );
+  return folders
+    ? folders
+        // フォルダデータの親フォルダ（parent_folder）が受け取ったparentIdと一致するフォルダをフィルター
+        // parentIdがない場合は親フォルダが存在しないフォルダ（第1階層のフォルダ）を取得する
+        .filter((folder) => folder.parent_relation.parent_folder === parentId)
+        .map((folder) => (
+          <li id={folder.id} key={folder.id} className="flex flex-col gap-5">
+            <FolderItem
+              folder={folder}
+              folderData={folders}
+              isSubFolderVisible={!!openFolders[folder.id]}
+              toggleFolder={() => toggleFolder(folder.id)}
+            />
+            {/* フォルダに子フォルダがある場合、再帰的にFolderTreeを呼び出す */}
+            {folder.parent_relation.hasChild && openFolders[folder.id] && (
+              <ul className="ml-6 flex flex-col gap-5">
+                <FolderTree
+                  folders={folders}
+                  parentId={folder.id}
+                  openFolders={openFolders}
+                  toggleFolder={toggleFolder}
+                />
+              </ul>
+            )}
+          </li>
+        ))
+    : null;
 };
 
 export default FolderTree;
