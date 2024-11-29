829deebe034a45cade1d04b571e02ee9c2a24106
keita7777 <47020745+keita7777@users.noreply.github.com>
2024-11-29 12:17:55 +0900

    relativeを追加 (#84)
    Co-authored-by: github keita7777 <github mine0kei@gmail.com>

--- components/Bookmark/List/Card/index.tsx
+++ components/Bookmark/List/Card/index.tsx
@@ -15,7 +15,7 @@ const BookmarkCard = async ({ bookmark }: Props) => {
 
   return (
     <li id={bookmark.id} className="flex flex-col border border-black rounded-md p-3 relative gap-4">
-      <div className="flex justify-between items-center gap-4">
+      <div className="flex justify-between items-center gap-4 relative">
         <p className="bg-blue-300 flex justify-start items-center px-2 py-1 rounded-md text-xs w-max">
           {breadcrumbKeys
             .filter((key) => breadcrumb[key]?.name)
