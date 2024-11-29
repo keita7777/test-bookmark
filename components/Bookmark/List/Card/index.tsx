a1603b652dcd638f7daefbae1efc615a08fc7840
keita7777 <47020745+keita7777@users.noreply.github.com>
2024-11-29 10:19:58 +0900

    ブックマークカードの設定ボタン位置変更 (#75)
    Co-authored-by: github keita7777 <github mine0kei@gmail.com>

--- components/Bookmark/List/Card/index.tsx
+++ components/Bookmark/List/Card/index.tsx
@@ -15,15 +15,18 @@ const BookmarkCard = async ({ bookmark }: Props) => {
 
   return (
     <li id={bookmark.id} className="flex flex-col border border-black rounded-md p-3 relative gap-4">
-      <p className="bg-blue-300 flex justify-start items-center px-2 py-1 rounded-md text-xs w-max">
-        {breadcrumbKeys
-          .filter((key) => breadcrumb[key]?.name)
-          .map((key, index, array) => (
-            <span key={key} className={`${index < array.length - 1 ? "after:content-['/'] after:mx-2" : ""}`}>
-              {breadcrumb[key]?.name}
-            </span>
-          ))}
-      </p>
+      <div className="flex justify-between items-center gap-4">
+        <p className="bg-blue-300 flex justify-start items-center px-2 py-1 rounded-md text-xs w-max">
+          {breadcrumbKeys
+            .filter((key) => breadcrumb[key]?.name)
+            .map((key, index, array) => (
+              <span key={key} className={`${index < array.length - 1 ? "after:content-['/'] after:mx-2" : ""}`}>
+                {breadcrumb[key]?.name}
+              </span>
+            ))}
+        </p>
+        <SettingButton id={bookmark.id} />
+      </div>
       <div className="flex flex-col xl:flex-row">
         <a
           href={bookmark.url}
@@ -34,10 +37,7 @@ const BookmarkCard = async ({ bookmark }: Props) => {
           <Image src={bookmark.image || testImage} fill alt="画像" />
         </a>
         <div className="flex flex-col gap-2 w-full xl:ml-5">
-          <div className="flex justify-between relative">
-            <h2 className="text-xl font-bold">{bookmark.title}</h2>
-            <SettingButton id={bookmark.id} />
-          </div>
+          <h2 className="text-xl font-bold">{bookmark.title}</h2>
 
           <p className="text-gray-600">{bookmark.description}</p>
         </div>
