428c01038e58d502e74457af7dbf236f5b5b4fc9
keita7777 <47020745+keita7777@users.noreply.github.com>
2024-12-02 22:52:25 +0900

    認証状態によってページを保護 (#145)
    Co-authored-by: github keita7777 <github mine0kei@gmail.com>

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,10 +1,20 @@
 // (auth)配下のページは認証フォームのみ表示
 
-export default function BookmarkLayout({
+import { auth } from "@/utils/auth/auth";
+import { redirect } from "next/navigation";
+
+export default async function BookmarkLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  // ログインしている場合はホーム画面に遷移させる
+  const session = await auth();
+
+  if (session?.user?.id) {
+    redirect("/");
+  }
+
   return (
     <div className="h-full min-h-screen">
       <main className="p-4 flex justify-center items-center">{children}</main>
--- app/(bookmark)/layout.tsx
+++ app/(bookmark)/layout.tsx
@@ -3,12 +3,21 @@
 
 import Header from "@/components/Header";
 import { OpenMenuProvider } from "@/context/OpenMenuContext";
+import { auth } from "@/utils/auth/auth";
+import { redirect } from "next/navigation";
 
-export default function BookmarkLayout({
+export default async function BookmarkLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  // ログインしていない場合は/signinページに遷移させる
+  const session = await auth();
+
+  if (!session?.user?.id) {
+    redirect("/signin");
+  }
+
   return (
     <div className="flex flex-col md:flex-row h-full min-h-screen">
       <OpenMenuProvider>
